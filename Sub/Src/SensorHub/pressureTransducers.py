9f6181fd338181619ca136b5b2574fbdd9705fdb
David Walker-Howell <piercedhowell@gmail.com>
2018-12-22 16:51:56 -0800

    Wrote a basic outline of the PressureTransducer class that is used to retrieve pressure and depth data from each pressure transducer.

---
+++ Sub/Src/SensorHub/pressureTransducers.py
@@ -0,0 +1,109 @@
+'''
+Copyright 2018, David Pierce Walker-Howell, All rights reserved
+
+Author: David Pierce Walker-Howell<piercedhowell@gmail.com>
+Last Modified 12/22/2018
+Description: This module is the driver of the pressure transducers on the sub
+                used for determining depth.
+'''
+import sys
+import os
+PROTO_PATH = os.path.join("..", "..", "..", "Proto")
+sys.path.append(os.path.join(PROTO_PATH, "Src"))
+sys.path.append(PROTO_PATH)
+
+from MechOS import mechos
+from sensorHub import SensorHubBase
+import serial
+import time
+import struct
+from protoFactory import packageProtobuf
+import Mechatronics_pb2
+
+class PressureTransducer:
+    '''
+    The Pressure transducer class is used to communicate with the pressure
+    transducers on the sub via serial. The data packets received from a
+    transducer will be unpacked and converted to pressure or depth data. However,
+    the transducers will need to be calibrated at each use.
+    '''
+
+    def __init__(self, com_port, baud_rate, id):
+        '''
+        Initialize serial connection with a Pressure Transducer. Since multiple
+        transducers may exist on the system, provide an ID for each transducer.
+
+        Parameters:
+            com_port: There serial communication port used to get data from the
+                        pressure transducer.
+            baud_rate: The baud rate to communicate with the transducers.
+            id: An integer ID for the given transducer. By default this is 1, but
+                if you are using multiple transducers, give each a unique id.
+        Returns:
+            N/A
+        '''
+        self.com_port = com_port
+        self.transducer_serial = serial.Serial(com_port, baud_rate)
+        self.id = id
+
+        #initially set the pressure and depth_error to zero, this should be reset
+        #by the calibration function
+        self.pressure_error = 0
+        self.depth_error = 0
+
+        #TODO: Figure out these values!
+        #scaling factor to convert raw data to pressure data
+        self.pressure_scaling = 1
+
+        #scaling factor to convert raw data to depth data
+        self.depth_scaling = 1
+    def get_pressure(self):
+        '''
+        Return the pressure in measurement of PSI.
+
+        Parameters:
+            N/A
+        Returns:
+            pressure_data: The pressure data measured in PSI
+            None: If data cannot be retrieved, return None
+        '''
+        raw_pressure_data = self._unpack()
+
+        #convert raw_pressure_data to pressure in PSI
+        pressure_data = (raw_pressure_data * self.pressure_scaling) - self.pressure_error
+
+    def get_depth(self):
+        '''
+        Return the depth of the AUV in feet.
+
+        Parameters:
+            N/A
+        Returns:
+            depth_data: The depth data measured in feet
+            None: If the data cannot be retrieved, return None
+        '''
+        raw_pressure_data = self._unpack()
+        pressure_data = (raw_pressure_data * self.depth_scaling) - self.depth_error
+
+    def calibrate(self):
+        pass
+
+    def _unpack(self):
+        '''
+        Retrieve a single data packet from the transducer and unpack it into
+        numberical form.
+
+        Parameters:
+            N/A
+        Returns:
+            raw_pressure_data: The raw pressure data received in the data packet.
+            None: If an error occurs and/or no pressure data was received.
+        '''
+        try:
+            if self.transducer_serial > 0:
+                raw_pressure_data = int(self.transducer_serial.readline())
+                return raw_pressure_data
+        except RuntimeError:
+            print("Couldn't receive any data from Pressure Transducer", self.id,
+                    "on serial port", self.com_port)
+            return None
