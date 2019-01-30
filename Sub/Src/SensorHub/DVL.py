e963c74c3a7c37af0072a08108d5b5a795f919d5
christian <christian.d.gould@gmail.com>
2019-01-29 23:42:20 -0800

    DVL.py edited for usability and readability

--- Sub/Src/SensorHub/DVL.py
+++ Sub/Src/SensorHub/DVL.py
@@ -1,132 +1,96 @@
+'''
+Adapted from the origional dvl.py by Jared Guerrero, and in Accordance with Mechatronics Etiquite and
+the Creative Commons Licence LLC.
+
+Current Maintainers: Christian Gould and Cole Brower
+Email: Christian.d.gould@gmail.com
+Last Modified 1/29/2019
+'''
+import sys
+import os
+PROTO_PATH = os.path.join("..", "..", "..", "Proto")
+sys.path.append(os.path.join(PROTO_PATH, "Src"))
+sys.path.append(PROTO_PATH)
+
+from MechOS import mechos
 from sensorHub import SensorHubBase
 import serial
+import time
+import struct
+from protoFactory import packageProtobuf
+import Mechatronics_pb2
 
-class DVL(SensorHubBase):
+class DVL_Data_Driver:
+	def __init__(self, comport):
+		self.DVLCom = serial.Serial(comport, 115200, timeout=1)
 
-    def __init__(self):
-        super(DVL, self).__init__()
-        #Change type to match protobuf
-        self.type = "DVL_DATA"
-        self.serialObject = serial.Serial("/dev/ttyUSB5", 115200)
+	def Get_Velocity(self):
+		SYNC = 0;
+		while(SYNC != "0xa5"):
+			SYNC = hex(ord(self.DVLCom.read()))
+		header = ord(self.DVLCom.read())
+		ID     = ord(self.DVLCom.read())
+		family = ord(self.DVLCom.read())
+		dataSize = self.DVLCom.read(2)
+		dataChecksum = self.DVLCom.read(2)
+		headerChecksum = self.DVLCom.read(2)
 
-        #Taken from robosub-2018
-        self.alertList = []
-        self.getList = []
-        self.ahrs = 0
-        self.distanceToFloor = 0
-        self.velocitiesXYZ = [0, 0, 0]  # Velocities for the DVL's coordinate system
-        self.velTimesXYZ = [0, 0, 0]  # Time estimate for the velocities
-        self.positionA = [0, 0, 0]  # North, East, Down
+		if(hex(ID) == "0x1b"):
+			self.DVLCom.read(16)
 
+			error = self.DVLCom.read(4)
+			error = struct.unpack('<f', error)
 
-    def receiveSensorData(self):
-        return self.unpack()
+			self.DVLCom.read(112)
 
+			velX = self.DVLCom.read(4)
+			velX = struct.unpack('<f', velX)
+			velY = self.DVLCom.read(4)
+			velY = struct.unpack('<f', velY)
+			velZ = self.DVLCom.read(4)
+			velZ = struct.unpack('<f', velZ)
 
-    '''
-    Taken From robosub-2018
-    :Author Jared Guerrero
-    '''
-#TODO FIGURE OUT WHAT DATA WE NEED TO BE SENT. PROTOS STATE (up, x, y, err)
-    def unpack(self):
-        try:
-            if self.serialObject.inWaiting() != 0:
-                SYNC = hex(ord(self.serialObject.read()))
-                if SYNC == "0xa5":
-                    header = ord(self.serialObject.read())  # Use ord when dealing with 1 byte
-                    ID = ord(self.serialObject.read())  # How many bytes in ensemble
-                    family = ord(self.serialObject.read())  # How many bytes in ensemble
-                    dataSize = self.serialObject.read(2)  # Specify number of bytes if greater than 1
-                    dataChecksum = self.serialObject.read(2)  # How many bytes in ensemble
-                    headerChecksum = self.serialObject.read(2)  # How many bytes in ensemble
-                    if hex(ID) == "0x1b":  # If this is a Bottom Tracking Message
-                        version = ord(self.serialObject.read())
-                        offsetOfData = ord(self.serialObject.read())
-                        serialNumber = self.serialObject.read(4)
-                        year = ord(self.serialObject.read())
-                        month = ord(self.serialObject.read())
-                        day = ord(self.serialObject.read())
-                        hour = ord(self.serialObject.read())
-                        minute = ord(self.serialObject.read())
-                        seconds = ord(self.serialObject.read())
-                        microSec = self.serialObject.read(2)
-                        numberOfBeams = self.serialObject.read(2)
-                        error = self.serialObject.read(4)
-                        status = self.serialObject.read(4)
-                        soundSpeed = self.serialObject.read(4)
-                        temperature = self.serialObject.read(4)
-                        pressure = self.serialObject.read(4)
-                        velBeam0 = self.serialObject.read(4)
-                        velBeam1 = self.serialObject.read(4)
-                        velBeam2 = self.serialObject.read(4)
-                        velBeam3 = self.serialObject.read(4)
-                        disBeam0 = self.serialObject.read(4)
-                        disBeam0 = struct.unpack('<f', disBeam0)
-                        disBeam1 = self.serialObject.read(4)
-                        disBeam1 = struct.unpack('<f', disBeam1)
-                        disBeam2 = self.serialObject.read(4)
-                        disBeam2 = struct.unpack('<f', disBeam2)
-                        disBeam3 = self.serialObject.read(4)
-                        disBeam3 = struct.unpack('<f', disBeam3)
-                        self.distanceToFloor = ((disBeam0[0]) + (disBeam1[0]) + (disBeam2[0]) + (disBeam3[0])) / 4
-                        fomBeam0 = self.serialObject.read(4)
-                        fomBeam1 = self.serialObject.read(4)
-                        fomBeam2 = self.serialObject.read(4)
-                        fomBeam3 = self.serialObject.read(4)
-                        dt1Beam0 = self.serialObject.read(4)
-                        dt1Beam1 = self.serialObject.read(4)
-                        dt1Beam2 = self.serialObject.read(4)
-                        dt1Beam3 = self.serialObject.read(4)
-                        dt2Beam0 = self.serialObject.read(4)
-                        dt2Beam1 = self.serialObject.read(4)
-                        dt2Beam2 = self.serialObject.read(4)
-                        dt2Beam3 = self.serialObject.read(4)
-                        timeVelEstBeam0 = self.serialObject.read(4)
-                        timeVelEstBeam1 = self.serialObject.read(4)
-                        timeVelEstBeam2 = self.serialObject.read(4)
-                        timeVelEstBeam3 = self.serialObject.read(4)
-                        velX = self.serialObject.read(4)
-                        velX = struct.unpack('<f', velX)
-                        self.velocitiesXYZ[0] = velX[0]
-                        velY = self.serialObject.read(4)
-                        velY = struct.unpack('<f', velY)
-                        self.velocitiesXYZ[1] = velY[0]
-                        velZ1 = self.serialObject.read(4)
-                        velZ1 = struct.unpack('<f', velZ1)
-                        self.velocitiesXYZ[2] = velZ1[0]
-                        velZ2 = self.serialObject.read(4)
-                        fomX = self.serialObject.read(4)
-                        fomY = self.serialObject.read(4)
-                        fomZ1 = self.serialObject.read(4)
-                        fomZ2 = self.serialObject.read(4)
-                        dt1X = self.serialObject.read(4)
-                        dt1Y = self.serialObject.read(4)
-                        dt1Z1 = self.serialObject.read(4)
-                        dt1Z2 = self.serialObject.read(4)
-                        dt2X = self.serialObject.read(4)
-                        dt2Y = self.serialObject.read(4)
-                        dt2Z1 = self.serialObject.read(4)
-                        dt2Z2 = self.serialObject.read(4)
-                        timeVelEstX = self.serialObject.read(4)
-                        timeVelEstX = struct.unpack('<f', timeVelEstX)  #
-                        self.velTimesXYZ[0] = timeVelEstX[0]
-                        timeVelEstY = self.serialObject.read(4)
-                        timeVelEstY = struct.unpack('<f', timeVelEstY)
-                        self.velTimesXYZ[1] = timeVelEstY[0]
-                        timeVelEstZ1 = self.serialObject.read(4)
-                        timeVelEstZ1 = struct.unpack('<f', timeVelEstZ1)
-                        self.velTimesXYZ[2] = timeVelEstZ1[0]
-                        timeVelEstZ2 = self.serialObject.read(4)
+			self.DVLCom.flush()
+		return velZ[0], velX[0], velY[0], error[0]
 
-                    return velZ, velX, velY, error
+class DVL_Publisher(SensorHubBase):
+	'''
+	Communicates with the Nortek DVL and publishes Data to sensor hub to be routed to mission planner 
+	and movement control
+	'''
+
+	def __init__(self,comport):
+
+		super(DVL_Publisher,self).__init__()
+		#Change type to match protobuf type
+		self.type = "DVL_DATA"
+
+		#Create an Object for Nortec DVL data
+		self.DVL  = DVL_Data_Driver(comport);
+
+		#Define a MechOS node and publisher
+		self.sensorHub_DVL = mechos.Node("SENSORHUB_DVL")
+
+		#Override the parent publisher attribute
+		self.publisher = self.sensorHub_DVL.create_publisher("DVL_DATA")
+
+	def Velocity_TO_Displacement(self):
+		'''
+		Converts Velocity Data to Displacement data as Components: Dx,Dy,Dz
+		'''
+		pass;
 
 
-                else:
-                    self.serialObject.flushInput()
+	def publish(self):
+		while(1):
+			Z, X, Y, err = self.DVL.Get_Velocity()
+			self.data = [float(x) for x in [Z,X,Y,err]]
+			if( v_t != 0 for v_t in self.data):
+				self.publish_data()
+				print(self.data)
+			time.sleep(0.25)
 
-        except Exception as msg:
-            print("Can't receive data from DVL: {}".format(msg))
 
 if __name__ == "__main__":
-    dvl = DVL()
-    dvl.run()
+	DVL = DVL_Publisher('/dev/ttyUSB0');
+	DVL.publish()
