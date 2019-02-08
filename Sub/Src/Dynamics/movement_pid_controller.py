79ab3b2fbb156944d94e11efde958d2d74c9af6d
David Walker-Howell <piercedhowell@gmail.com>
2019-02-07 23:13:33 -0800

    Addedthe pid controller and pid based movement/thruster controller. However theses are unfinished and need significant testing. I had to remove duplicate files from a bad proto compiliation. Also renamed timer.py to util_timer.py since timer was already a native library to python.

---
+++ Sub/Src/Dynamics/movement_pid_controller.py
@@ -0,0 +1,165 @@
+import sys
+import os
+
+HELPER_PATH = os.path.join("..", "Helpers")
+sys.path.append(HELPER_PATH)
+import util_timer
+
+PROTO_PATH = os.path.join("..", "..", "..", "Proto")
+sys.path.append(os.path.join(PROTO_PATH, "Src"))
+sys.path.append(PROTO_PATH)
+from protoFactory import packageProtobuf
+import Mechatronics_pb2
+
+from thruster import Thruster
+from pid_controller import PID_Controller
+from MechOS import mechos
+
+class Movement_PID:
+    '''
+    '''
+
+    def __init__(self, maestro_serial_obj):
+        '''
+        '''
+
+        #Initialize all 8 thrusters (max thrust 80%)
+        self.thrusters = [None, None, None, None, None, None, None, None]
+        self.thrusters[0] = Thruster(maestro_serial_obj, 1, [0, 0, 1], [1, -1, 0], 80)
+        self.thrusters[1] = Thruster(maestro_serial_obj, 2, [0, 1, 0], [1, 0, 0], 80)
+        self.thrusters[2] = Thruster(maestro_serial_obj, 3, [0, 0, 1], [1, 1, 0], 80)
+        self.thrusters[3] = Thruster(maestro_serial_obj, 4, [1, 0, 0], [0, 1, 0], 80)
+        self.thrusters[4] = Thruster(maestro_serial_obj, 5, [0, 0, 1], [-1, 1, 0], 80)
+        self.thrusters[5] = Thruster(maestro_serial_obj, 6, [0, 1, 0], [-1, 0, 0], 80)
+        self.thrusters[6] = Thruster(maestro_serial_obj, 7, [0, 0, 1], [-1, -1, 0], 80)
+        self.thrusters[7] = Thruster(maestro_serial_obj, 8, [1, 0, 0], [0, -1, 0], 80)
+
+        #Initialize parameter server client to get and set parameters related to sub
+        self.param_serv = mechos.Parameter_Server_Client()
+
+        parameter_xml_database = os.path.join("..", "Params", "Perseverance.xml")
+        self.param_serv.use_parameter_database(parameter_xml_database)
+
+        #Initialize the PID controllers for control system
+        self.set_up_PID_controller()
+
+    def set_up_PID_controllers(self):
+        '''
+        Setup the PID controllers with the initial values set in the subs
+        parameter xml server file.
+
+        Parameters:
+            N/A
+
+        Returns:
+            N/A
+        '''
+        d_t = self.param_serv.get_param("PID/roll_pid/d_t")
+
+        roll_p = self.param_serv.get_param("PID/roll_pid/p")
+        roll_i = self.param_serv.get_param("PID/roll_pid/i")
+        roll_d = self.param_serv.get_param("PID/roll_pid/d")
+        self.roll_pid_controller = PID_Controller(roll_p, roll_i, roll_d, d_t)
+
+        pitch_p = self.param_serv.get_param("PID/pitch_pid/p")
+        pitch_i = self.param_serv.get_param("PID/pitch_pid/i")
+        pitch_d = self.param_serv.get_param("PID/pitch_pid/d")
+        self.pitch_pid_controller = PID_Controller(pitch_p, pitch_i, pitch_d, d_t)
+
+        yaw_p = self.param_serv.get_param("PID/yaw_pid/p")
+        yaw_i = self.param_serv.get_param("PID/yaw_pid/i")
+        yaw_d = self.param_serv.get_param("PID/yaw_pid/d")
+        self.yaw_pid_controller = PID_Controller(yaw_p, yaw_i, yaw_d, d_t)
+
+        x_p = self.param_serv.get_param("PID/x_pid/p")
+        x_i = self.param_serv.get_param("PID/x_pid/i")
+        x_d = self.param_serv.get_param("PID/x_pid/d")
+        self.x_pid_controller = PID_Controller(x_p, x_i, x_d, d_t)
+
+        y_p = self.param_serv.get_param("PID/y_pid/p")
+        y_i = self.param_serv.get_param("PID/y_pid/i")
+        y_d = self.param_serv.get_param("PID/y_pid/d")
+        self.y_pid_controller = PID_Controller(y_p, y_i, y_d, d_t)
+
+        #z = depth pid
+        z_p = self.param_serv.get_param("PID/z_pid/p")
+        z_i = self.param_serv.get_param("PID/z_pid/i")
+        z_d = self.param_serv.get_param("PID/z_pid/d")
+        self.z_pid_controller = PID_Controller(z_p, z_i, z_d, d_t)
+
+    def simple_thrust(self, thrusts):
+        '''
+        Individually sets each thruster PWM given the PWMs in the thrusts
+        list.
+
+        Parameters:
+            thrusts: A list of length 8 containing a thrust value (%) between
+            [-100, 100]. The list should be in order of the thruster ids.
+
+        Returns:
+            N/A
+        '''
+
+        for thruster_id, thrust in enumerate(thrusts):
+            self.thrusters[thruster_id].set_thrust(thrust)
+
+    def controlled_thrust(self, roll_control, pitch_control, yaw_control, x_control,
+                        y_control, z_control):
+        '''
+        Function takes control outputs from calculated by the PID values and applys
+        them to the 6 degree control matrix to determine the thrust for each thruster
+        to reach desired point.
+
+        Parameters:
+            roll_control: The control output from the roll pid controller.
+            pitch_control: The control output from the pitch pid controller.
+            yaw_control: The control output from the yaw pid controller.
+            x_control: The control output from the x translation pid controller.
+            y_control: The control output from the y translation pid controller.
+            z_control: The control output from the z translation pid controller.
+
+        Returns:
+            N/A
+        '''
+        for thruster_id, thruster in enumerate(self.thrusters):
+
+            thrust = (roll_control * thruster.orientation[2] * thruster.location[1]) +
+                     (pitch_control * thruster.orientation[2] * thruster.location[0]) +
+                     (yaw_control * thruster.orientation[1] * thruster.location[0]) +
+                     (yaw_contorl * thruster.orientation[0] * thruster.location[1]) +
+                     (x_pwm * thruster.orientation[0]) +
+                     (y_pwm * thruster.orientation[1]) +
+                     (z_pwm * thruster.orientation[2])
+            self.thrusters[thruster_id].set_thrust(thrust)
+
+    def advanceMove(self, curr_roll, curr_pitch, curr_yaw, curr_x_pos, curr_y_pos,
+                    curr_z_pos, desired_roll, desired_pitch, desired_yaw,
+                    desired_x_pos, desired_y_pos, desired_z_pos):
+        '''
+        Given the current position and desired positions of the AUV, obtain the pid
+        control outputs for all 6 degrees of freedom.
+
+        Parameters:
+            curr_roll: Current roll data
+            curr_pitch Current pitch data
+            curr_yaw: Current yaw data
+            curr_x_pos: Current x position
+            curr_y_pos: Current y position
+            curr_z_pos: Current z position
+            desired_roll: Desired roll position
+            desired_pitch: Desired pitch position
+            desired_yaw: Desired yaw position
+            desired_x_pos: Desired x position
+            desired_y_pos: Desired y position
+            desired_z_pos: Desired z position
+
+        Returns:
+            roll_control: The control output from the roll pid controller.
+            pitch_control: The control output from the pitch pid controller.
+            yaw_control: The control output from the yaw pid controller.
+            x_control: The control output from the x translation pid controller.
+            y_control: The control output from the y translation pid controller.
+            z_control: The control output from the z translation pid controller.
+        '''
+
+        pass
---
+++ Sub/Src/Dynamics/pid_controller.py
@@ -0,0 +1,98 @@
+'''
+Copyright 2019, David Pierce Walker-Howell, All rights reserved
+
+Author: David Pierce Walker-Howell<piercedhowell@gmail.com>
+Last Modified 02/05/2019
+Description: This module contains a PID controller class that is
+            used for feedback control systems.
+'''
+
+import sys
+import os
+
+HELPER_PATH = os.path.join("..", "Helpers")
+sys.path.append(HELPER_PATH)
+import util_timer
+
+import time
+
+
+class PID_Controller():
+    '''
+    PID controller typically used in control feed back systems.
+    '''
+    def __init__(self, k_p, k_i, k_d, d_t, l_bound=None, u_bound=None):
+        '''
+        Initialize the the PID controller parameters.
+
+        Parameters:
+            k_p: Proportional gain
+            k_i: Integral gain
+            k_d: Derivative gain
+            d_t: Time interval between calculating to output control
+            l_bound: Lower bound for PID output
+            u_bound: Upper bound for PID output
+        Returns:
+            N/A
+        '''
+        self.k_p = k_p
+        self.k_i = k_i
+        self.k_d = k_d
+        self.d_t = d_t
+        self.l_bound = l_bound
+        self.u_bound = u_bound
+
+        self.integral = 0
+        self.previous_error = 0
+
+        #Timer to check difference time in between control calculations
+        self.PID_timer = util_timer.Timer()
+
+
+    def set_gains(self, k_p, k_i, k_d, d_t=None):
+        '''
+        Reset the gain parameters.
+
+        Parameters:
+            k_p: Proportional gain
+            k_i: Integral gain
+            k_d: Derivative gain
+            d_t: Time interval between calculating to output control.
+                    If no value is passed, it will stay what it originally
+                    was set to during initialization.
+        '''
+        self.k_p = k_p
+        self.k_i = k_i
+        self.k_d = k_d
+        self.d_t = d_t
+    def control_step(self, error):
+        '''
+        Perfrom a control step to correct for error in control system.
+
+        Parameters:
+            error: The error from current point to desired set point.
+
+        Returns:
+            PID: An PID output control value to correct for error in system
+        '''
+        P = self.k_p * error #proportional term
+
+        #Ensure semi time difference in between each control step
+        calc_time_interval = self.PID_timer.net_timer()
+        if( calc_time_interval < self.d_t):
+            time.sleep((self.d_t - calc_timer_interval))
+            self.PID_timer.restart_timer()
+
+        self.integral = self.integral + (error * self.d_t)
+        I = self.k_i * self.integral
+
+        D = self.k_d * (error - self.previous_error) / self.d_t
+
+        PID = P + I + D
+        
+        if(PID < self.l_bound):
+            PID = self.l_bound
+        elif(PID > self.u_bound):
+            PID = self.u_bound
+
+        return PID
