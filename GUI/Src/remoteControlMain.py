c93c6f6335a515b1e01505e4c474702fdef1d39b
Christian <christian.d.gould@gmail.com>
2019-02-22 17:29:43 -0800

    making small changes to the imports for the remote control class

--- GUI/Src/remoteControlMain.py
+++ GUI/Src/remoteControlMain.py
@@ -2,12 +2,9 @@ import sys
 import os
 #\/\/CHECK LOCATION OF FILE!!!\/\/#
 
-PROTO_PATH = os.path.join("..", "..", "Proto") #<---- may need to change
 SHAFI_PATH = os.path.join("..","..","Sub","Src","Dynamics","Shafi")
 ALEXA_PATH = os.path.join("..","..","Sub","Src","Dynamics","Alexa")
-
-PROTO_PATH = os.path.join("Proto") #<---- may need to change
-sys.path.append(os.path.join(PROTO_PATH, "Src"))
+PROTO_PATH = os.path.join("..","..","Proto")
 sys.path.append(PROTO_PATH)
 sys.path.append(SHAFI_PATH)
 sys.path.append(ALEXA_PATH)
