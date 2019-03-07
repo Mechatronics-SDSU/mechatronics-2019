6daf8ce00c94046e38d89176a80850d7914235a1
David Walker-Howell <piercedhowell@gmail.com>
2019-03-07 11:25:20 -0800

    Added attributes to pressureTransducers to have the ability to access both the raw pressure data and non-kalman filtered depth data.

--- Sub/Src/SensorHub/pressureTransducers.py
+++ Sub/Src/SensorHub/pressureTransducers.py
@@ -102,11 +102,11 @@ class Pressure_Depth_Transducers:
             depths: A list of two depth readings in feet.
             if data is not received properly, return none
         '''
-
+        self.raw_depth_data = raw_pressure_data
         if(raw_pressure_data != None):
             depths = raw_pressure_data
             depths[0] = (1 / self.depth_scaling[0]) * (depths[0] - self.depth_bias[0])
             depths[1] = (1 / self.depth_scaling[1]) * (depths[1] - self.depth_bias[1])
-
+            self.unfiltered_depth_data = depths
             return depths
         return None
