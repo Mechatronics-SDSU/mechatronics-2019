7b30ec0f777d299cad3f8cbe43454a4916899393
PierceATronics <piercedhowell@gmail.com>
2019-06-01 13:02:22 -0700

    Deleting the unwanted mission related files.

--- Sub/Src/Mission/missionTask.py
+++
@@ -1,5 +0,0 @@
-class MissionTask(object):
-    def __init__(self, json_file):
-        self.type = json_file["type"]
-        self.estimated_position = dict(json_file["estimated_position"])
-        self.timeout = json_file["timeout"]
