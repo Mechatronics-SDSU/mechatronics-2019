39f4dd4234ef185528e5fba2e3a6447c0c22274e
Christian Gould <christian.d.gould@gmail.com>
2019-06-10 13:16:42 -0700

    Updating network.py to implement UDP socket protocol

--- Sub/Src/Dynamics/message_passing/communicationUtils/network.py
+++ Sub/Src/Dynamics/message_passing/communicationUtils/network.py
@@ -4,17 +4,28 @@ class publisher(object):
 		'''
 		Ip router is dictionary. key: ip address value: list of ports
 		'''
-		pass
+		self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def publish(self, address, msg):
-		pass
-
+	def publish(self, address, msg, type="UDP"):
+		if type.upper() == "TCP":
+			raise IOError(type, 'Functionality not implemented')
+		return self.UDP_socket.sendto(msg, address)
+		
 class subscriber(object):
 	def __init__(self, ip_router):
 		'''
 		Ip router is dictionary. key: ip address value: list of ports
+		further segregation in types UDP or TCP
 		'''
-		pass
+		self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 	def subscribe(self, address):
-		pass
+		'''
+		Subscribes to address provided
+		'''
+		self.UDP_socket.bind(address)
+		data, addr = sock.recvfrom(1024)
+		
+		return data
