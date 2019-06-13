afdd39351d969c4d6a22306bc6411e0669cb0516
mashafi <ma.shafi99@gmail.com>
2019-06-12 21:16:10 -0700

    debugs for nodebase

--- Sub/Src/Dynamics/message_passing/Nodes/node_base.py
+++ Sub/Src/Dynamics/message_passing/Nodes/node_base.py
@@ -34,9 +34,8 @@ class node_base(ABC, threading.Thread):
             :returns: 0 on sucess 1 on error, errors are logged to outputbuffer
         '''
 
-        if foreign_address:
-            type = foreign_address[0:3] #check if udp or tcp
-            self._publisher.publish(foreign_address, msg, type)
+        if foreign_address is not None:
+            self._publisher.publish(msg, foreign_address)
 
         self._writer.write(local_address, msg)
 
@@ -49,8 +48,7 @@ class node_base(ABC, threading.Thread):
             :param str address: recv from local address as a string to the dictionary 'x_velocity' etc.
         '''
         if not local:
-            type = foreign_address[0:3] #check if udp or tcp
-            return self._subscriber.subscribe(address, type)
+            return self._subscriber.subscribe(address)
         else:
             return self._reader.read(address)
 
@@ -63,7 +61,7 @@ if __name__=='__main__':
 
     class WriteNode(node_base):
         def __init__(self, IP, MEM):
-            node_base.__init__(self, IP, MEM)
+            node_base.__init__(self, MEM, IP)
             self._memory = MEM
             self._ip_route = IP
             self.MSG='uninitialized'
@@ -78,14 +76,38 @@ if __name__=='__main__':
             start_time = time.time()
             while True:
                 if ( (time.time() - start_time) >= self.baud ):
-                    self._send(msg=self.MSG, local_address='Encrypted_dat')
+                    self._send(local_address='Encrypted_dat', msg = self.MSG)
                     start_time=time.time()
+                else:
+                    time.sleep(0)
+
+    class PublishNode(node_base):
+        def __init__(self, IP, MEM):
+            node_base.__init__(self, MEM, IP)
+            self._memory = MEM
+            self._ip_route = IP
+            self.address = list(IP)[0]
+            print(self._ip_route)
+            self.MSG = 'uninitialized'
+            self.baud=.128
+
+        def set_message(self, message):
+            self.MSG = message
+        def set_baudrate(self, baudrate):
+            self.baud = baudrate
+
+        def run(self):
+            start_time = time.time()
+            while True:
+                if((time.time() - start_time) >= self.baud):
+                    self._send(msg=self.MSG, local_address = 'Doesnt matter', foreign_address = IP)
+                    start_time = time.time()
                 else:
                     time.sleep(0)
 
     class ReadNode(node_base):
         def __init__(self, IP, MEM):
-            node_base.__init__(self, IP, MEM)
+            node_base.__init__(self, MEM, IP)
             self._memory = MEM
             self._ip_route = IP
             self.baud=.128
@@ -99,16 +121,41 @@ if __name__=='__main__':
                 else:
                     time.sleep(0)
 
+    class SubscribeNode(node_base):
+        def __init__(self, IP, MEM):
+            node_base.__init__(self, MEM, IP)
+            self._memory = MEM
+            self._ip_route = IP
+            self.baud=.128
+            self.address = list(IP)[0]
+            print(self._ip_route)
+
+        def run(self):
+            start_time = time.time()
+            while True:
+                if ((time.time() - start_time) >= self.baud):
+                    print(self._recv(IP, local=False))
+                    start_time = time.time()
+                else:
+                    time.sleep(0)
+
     # Volatile Memory Instances
-    IP={'tcp://127.0.0.101':5558}
+    IP={'127.0.0.101':5558}
     MEM={'Velocity_x':12.01,'Velocity_y':12.02,'Velocity_z':12.03,'Encrypted_dat':'None'}
 
     # Initialize Node
     MyWriteNode = WriteNode(IP, MEM)
+    #print(MyWriteNode._ip_route)
+    MyPublishNode = PublishNode(IP, MEM)
     MyReadNode  = ReadNode(IP, MEM)
+    MySubscribeNode = SubscribeNode(IP, MEM)
+    #print(MyReadNode._ip_route)
 
     # Start Thread
     MyWriteNode.start()
+    MyPublishNode.start()
     MyReadNode.start()
+    MySubscribeNode.start()
 
     MyWriteNode.set_message('Testing Message...initialized')
+    MyPublishNode.set_message('This is also a testing message')
--- Sub/Src/Dynamics/message_passing/communicationUtils/network.py
+++ Sub/Src/Dynamics/message_passing/communicationUtils/network.py
@@ -8,12 +8,17 @@ class publisher(object):
 		self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def publish(self, address, msg, type="UDP"):
+	def publish(self, msg, address, type = "udp"):
+		#host = address[0]
+		#type = host[0:3]
+		host = list(address.keys())[0]
+		port = address[host]
 		if type.upper() == "TCP":
-			self.TCP_socket.connect(address)
-			self.TCP_socket.send(msg.encode())
+			self.TCP_socket.connect((address))
+			self.TCP_socket.send(msg.encode)
 			#raise IOError(type, 'Functionality not implemented')
-		return self.UDP_socket.sendto(msg, address)
+		else:
+			self.UDP_socket.sendto(msg.encode(), (host, port))
 
 class subscriber(object):
 	def __init__(self, ip_router):
@@ -24,17 +29,21 @@ class subscriber(object):
 		self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	def subscribe(self, address, type):
+	def subscribe(self, address, type = "udp"):
 		'''
 		Subscribes to address provided
 		'''
+		#host = address[0]
+		#type = host[0:3]
 		if type.upper() == "TCP":
-			self.TCP_socket.bind(address)
+			self.TCP_socket.bind((address))
 			(cl, addr) = self.TCP_socket.accept()
 			data = cl.recv(1024).decode()
 			return data
 
-		self.UDP_socket.bind(address)
-		data, addr = self.UDP_socket.recvfrom(1024)
-
-		return data
+		else:
+			host = list(address.keys())[0]
+			port = address[host]
+			self.UDP_socket.bind((host, port))
+			data, addr = self.UDP_socket.recvfrom(1024)
+			return data.decode()
