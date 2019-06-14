00395a5ea6c36a38b18da628e6b6bb8fd5b94aea
mashafi <ma.shafi99@gmail.com>
2019-06-13 18:09:08 -0700

    Subscribe/publish works now

--- Sub/Src/Dynamics/message_passing/Nodes/node_base.py
+++ Sub/Src/Dynamics/message_passing/Nodes/node_base.py
@@ -5,6 +5,7 @@ sys.path.append(comm_path)
 
 import time
 import threading
+import socket
 
 from communicationUtils import local as local
 from communicationUtils import network as network
@@ -86,9 +87,11 @@ if __name__=='__main__':
             node_base.__init__(self, MEM, IP)
             self._memory = MEM
             self._ip_route = IP
+            '''
             self.address = list(IP)[0]
             print(self._ip_route)
-            self.MSG = 'uninitialized'
+            '''
+            self.MSG = b'uninitialized'
             self.baud=.128
 
         def set_message(self, message):
@@ -100,7 +103,7 @@ if __name__=='__main__':
             start_time = time.time()
             while True:
                 if((time.time() - start_time) >= self.baud):
-                    self._send(msg=self.MSG, local_address = 'Doesnt matter', foreign_address = IP)
+                    self._send(msg=self.MSG, local_address = 'Doesnt matter', foreign_address = ('127.0.0.101', 5558))
                     start_time = time.time()
                 else:
                     time.sleep(0)
@@ -127,20 +130,28 @@ if __name__=='__main__':
             self._memory = MEM
             self._ip_route = IP
             self.baud=.128
+
+            '''
             self.address = list(IP)[0]
             print(self._ip_route)
+            '''
 
         def run(self):
             start_time = time.time()
             while True:
                 if ((time.time() - start_time) >= self.baud):
-                    print(self._recv(IP, local=False))
+                    print(self._recv(('127.0.0.101', 5558), local=False))
                     start_time = time.time()
                 else:
                     time.sleep(0)
 
     # Volatile Memory Instances
-    IP={'127.0.0.101':5558}
+    pub_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sub_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    ip_address = ('127.0.0.101', 5558)
+    sub_socket.bind((ip_address))
+    #Dictionary with key: ip address, value is tuple with publisher/subscriber socket
+    IP={ip_address:(pub_socket, sub_socket)}
     MEM={'Velocity_x':12.01,'Velocity_y':12.02,'Velocity_z':12.03,'Encrypted_dat':'None'}
 
     # Initialize Node
@@ -158,4 +169,4 @@ if __name__=='__main__':
     MySubscribeNode.start()
 
     MyWriteNode.set_message('Testing Message...initialized')
-    MyPublishNode.set_message('This is also a testing message')
+    MyPublishNode.set_message(b'This is also a testing message')
--- Sub/Src/Dynamics/message_passing/communicationUtils/network.py
+++ Sub/Src/Dynamics/message_passing/communicationUtils/network.py
@@ -5,22 +5,26 @@ class publisher(object):
 		'''
 		Ip router is dictionary. key: ip address value: list of ports
 		'''
-		self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		#self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-		self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 		self.ip_router=ip_router
 		self.type=type
 
-	def publish(self, msg, register):
+	def publish(self, msg, key):
 
-		address = self.ip_router[register]
+		tuple = self.ip_router[key]
 
+		tuple[0].sendto(msg, key)
+
+		'''
 		if self.type.upper() == "TCP":
 
 			self.TCP_socket.sendto(msg, address)
 		else:
 			self.UDP_socket.sendto(msg, address)
+		'''
 
 class subscriber(object):
 	def __init__(self, ip_router, type):
@@ -28,28 +32,16 @@ class subscriber(object):
 		Ip router is dictionary. key: ip register value: list of ports
 		further segregation in types UDP or TCP
 		'''
-		self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-		self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		#self.UDP_socket=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		#self.TCP_socket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.ip_router=ip_router
 		self.type=type
 
 
-	def subscribe(self, register):
+	def subscribe(self, key):
 		'''
 		Subscribes to register provided
 		'''
-		address = self.ip_router[register]
-
-		if self.type.upper() == "TCP":
-
-			self.TCP_socket.bind(address)
-			data, addr = self.TCP_socket.recvfrom(4096)
-
-			return data
-
-		else:
-
-			self.UDP_socket.bind(address)
-			data, addr = self.UDP_socket.recvfrom(4096)
-
-			return data
+		tuple = self.ip_router[key]
+		data, addr = tuple[1].recvfrom(4096)
+		return data
