Pool Testing 06/15/2019. Need to change you the parameters update on the PID contollers so it doesn't slow down the system. Also need to fix the yaw calculation.

Sub/Src/Dynamics/thruster.py
@@ -63,7 +63,6 @@ class Thruster():
         self.orientation = orientation
         self.location = location
         self.max_thrust = max_thrust
-
         self.invert_thruster = 1
         if(invert_thruster):
             self.invert_thruster = -1
@@ -95,7 +94,7 @@ class Thruster():
                 #Re-bound thrust from [-100, 100] to [0, 255] for writing PWM signal
                 thrust = int(np.interp(self.max_thrust, [-100, 100], [0, 254]))
 
-            elif thrust < (-self.max_thrust):
+            elif thrust < (-1*self.max_thrust):
 
                 self.previous_thurst = copy.copy(-self.max_thrust)
 

Sub/Src/SensorHub/DVL.py
@@ -71,6 +71,9 @@ class DVL_DATA_DRIVER:
         velZ = [0]
         velX = [0]
         velY = [0]
+        timeVelEstZ = 0
+        timeVelEstX = 0
+        timeVelEstY = 0
         while(not SYNC_flag):
         #if(self.DVLCom.inWaiting() > 154):
             SYNC = hex(ord(self.DVLCom.read()))
@@ -113,11 +116,11 @@ class DVL_DATA_DRIVER:
         # conversion to ft/s
         if inFeet==True:
             velocities = np.array([velZ[0], velX[0], velY[0]]) * 3.28084
-            vel_times = np.array([timeVelEstZ, timeVelEstX, timeVelEstY]))
+            vel_times = np.array([timeVelEstZ, timeVelEstX, timeVelEstY])
             return(np.concatenate((velocities, vel_times), axis=None))
         # normal return as m/s
-        return (np.array([velZ[0], velX[0], velY[0]]), , np.array([timeVelEstZ, timeVelEstX, timeVelEstY]))
-"""
+        return (np.array([velZ[0], velX[0], velY[0]]), np.array([timeVelEstZ, timeVelEstX, timeVelEstY]))
+    """
     DEPRECATE THIS FUNCTION. Position calcs moved to sensor_driver.py
     def __get_displacement(self):
         '''
@@ -154,7 +157,7 @@ class DVL_DATA_DRIVER:
         '''
 
         return np.concatenate((self.curr_vel, self.displacement), axis=None)
-"""
+    """
     def get_PACKET(self):
         return self.__get_velocity()
 