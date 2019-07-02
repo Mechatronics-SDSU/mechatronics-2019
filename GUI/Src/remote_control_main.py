7093b3f9dbfbfd01c7af4ca34edec25a04046d22
Claire <clairefiorino@icloud.com>
2019-07-01 23:06:10 -0700

    Fixed layout of widgets on RC tab- still needs work

--- GUI/Src/remote_control_main.py
+++ GUI/Src/remote_control_main.py
@@ -20,7 +20,6 @@ from PyQt5.QtWidgets import QHBoxLayout, QLabel, QCheckBox, QStackedWidget
 from PyQt5.QtCore import QTimer, Qt
 from PyQt5.QtGui import QColor, QFont
 
-from controller_config_widget import Controller_Config
 from controller_status_widget import Joystick_Test
 from controller_diagram import Controller_Diagram
 from waypoint_widget import Waypoint_GUI
@@ -43,7 +42,7 @@ class Remote_Control_Widget(QWidget):
 		self.main_layout = QGridLayout(self)
 		self.secondary_layout = QVBoxLayout()
 
-		self.main_layout.addLayout(self.secondary_layout, 0, 0)
+		self.main_layout.addLayout(self.secondary_layout, 1, 0)
 		self.setGeometry(0,0,1000,700)
 
 		#Set the background color of the widget
@@ -54,10 +53,8 @@ class Remote_Control_Widget(QWidget):
 		#Place the controller config widget and the controller status widget on GUI
 		self.set_gui_label()
 		self.set_controller_status()
-		self.Stack = QStackedWidget(self)
-		self.controller_diagram = Controller_Diagram()
-		self.Stack.addWidget(self.controller_diagram)
-		self.secondary_layout.addWidget(self.Stack)
+		self.set_controller_diagram()
+		self.set_waypoint_gui()
 		
 	def set_gui_label(self):
 		'''
@@ -66,7 +63,7 @@ class Remote_Control_Widget(QWidget):
 		rc_title = QLabel("<font color='white'>Remote Control Mode</font>")
 		rc_title.setFont(QFont('SansSerif', 30))
 		rc_title.setAlignment(Qt.AlignCenter)
-		self.secondary_layout.addWidget(rc_title, 0)
+		self.main_layout.addWidget(rc_title, 0, 0)
 
 	def set_controller_status(self):
 		'''
@@ -74,8 +71,8 @@ class Remote_Control_Widget(QWidget):
 		'''
 		self.controller_stat = Joystick_Test()
 		optimal_size = self.controller_stat.sizeHint()
-		self.controller_stat.setMaximumSize(optimal_size)
-		self.secondary_layout.addWidget(self.controller_stat, 0)
+		#self.controller_stat.setMaximumSize(optimal_size)
+		self.main_layout.addWidget(self.controller_stat, 0, 1)
 
 	def set_controller_config(self):
 		'''
@@ -84,7 +81,7 @@ class Remote_Control_Widget(QWidget):
 		self.controller_conf = Controller_Config()
 		optimal_size = self.controller_conf.sizeHint()
 		self.controller_conf.setMaximumSize(optimal_size)
-		self.secondary_layout.addWidget(self.controller_conf, 0)
+		self.secondary_layout.addWidget(self.controller_conf, 2)
 
 	def set_controller_diagram(self):
 		'''
@@ -92,17 +89,18 @@ class Remote_Control_Widget(QWidget):
 		'''
 		self.controller_diag = Controller_Diagram()
 		optimal_size = self.controller_diag.sizeHint()
-		self.controller_diag.setMaximumSize(optimal_size)
+		self.controller_diag.setMinimumSize(600,500)
 		self.secondary_layout.addWidget(self.controller_diag, 0)
 
-    def set_waypoint_gui(self):
-        '''
-        Sets waypoint widget
-        '''
-        self.waypoint_gui = Waypoint_GUI()
-        optimal_size = self.waypoint_gui.sizeHint()
-        self.waypoint_gui.setMaximumSize(optimal_size)
-        self.secondary_layout.addWidget(self.waypoint_gui,0)
+
+	def set_waypoint_gui(self):
+		'''
+		Sets waypoint widget
+		'''
+		self.waypoint_gui = Waypoint_GUI()
+		optimal_size = self.waypoint_gui.sizeHint()
+		#self.waypoint_gui.setMaximumSize(optimal_size)
+		self.secondary_layout.addWidget(self.waypoint_gui, 1)
 
 if __name__ == "__main__":
 
