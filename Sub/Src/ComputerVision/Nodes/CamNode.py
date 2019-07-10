627e222c342727c59f105b7768d6d3fae8e85a00
christian gould <christian.d.gould@gmail.com>
2019-07-09 23:55:28 -0700

    adding necessities for yolo

--- Sub/Src/ComputerVision/Nodes/CamNode.py
+++ Sub/Src/ComputerVision/Nodes/CamNode.py
@@ -1,11 +1,15 @@
 import numpy as np
 import cv2
+import math
+import random
 import socket
 import sys
 import struct
 import math
 import io
 import time
+from ctypes import *
+from yolo_wrapper import *
 
 # Port Information
 HOST    = '192.168.1.20'
@@ -23,30 +27,47 @@ cap = cv2.VideoCapture(1)
 cap.set(3, 450)
 cap.set(4, 450)
 
+# load yolo here
+net = load_net(b"cfg/yolov3-tiny.cfg",
+               b"weights/yolov3-tiny_200.weights",
+               0)
+meta = load_meta(b"cfg/coco.data")
+fps = cap.get(cv2.CAP_PROP_FPS)
+print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
 while(True):
     # Capture frame-by-frame
-    ret, frame = cap.read()
+    ret, byte_frame = cap.read()
+
 
 #    input('press enter to continue...')
     # Check Video Size
 #    print(frame.shape[:2])
 
     # print('NO imencode',sys.getsizeof(frame))
-    # Capture Bytes
-    ret, byte_frame = cv2.imencode( '.jpg', frame )
 
     # Operations on the frame
-
     '''
-    None: No operations on Frame
+    Yolo: Operations on Frame For Yolo
     '''
+    if ret:
+        r = detect(net, meta, byte_frame)
 
-    # Get the Size of the image
-    image_size = sys.getsizeof(byte_frame)
+        for i in r:
+            x, y, w, h = i[2][0], i[2][1], i[2][2], i[2][3]
+            xmin, ymin, xmax, ymax = convertBack(float(x), float(y), float(w), float(h))
+            pt1 = (xmin, ymin)
+            pt2 = (xmax, ymax)
+            cv2.rectangle(byte_frame, pt1, pt2, (0, 255, 0), 2)
+            cv2.putText(byte_frame, i[0].decode() + " [" + str(round(i[1] * 100, 2)) + "]", (pt1[0], pt1[1] + 20), cv2.FONT_HERSHEY_SIMPLEX, 1, [0, 255, 0], 4)
+        # Get the Size of the image
+        image_size = sys.getsizeof(byte_frame)
 
-    # Max Byte Size
-    if image_size:
+    # Capture Bytes
+    ret, byte_frame = cv2.imencode( '.jpg', byte_frame )
 
+    # Sending The Frame
+    if ret:
         imageBuffer = io.BytesIO(byte_frame)
 
         number_of_packets = math.ceil(image_size/MAX_UDP_PACKET_SIZE)
@@ -83,4 +104,3 @@ while(True):
 
 # When everything done, release the capture
 cap.release()
-
---
+++ Sub/Src/ComputerVision/Nodes/yolo_wrapper.py
@@ -0,0 +1,186 @@
+import numpy as np
+import cv2
+import math
+import random
+import socket
+import sys
+import struct
+import math
+import io
+import time
+from ctypes import *
+
+#---STRUCTS---#
+
+def sample(probs):
+    s = sum(probs)
+    probs = [a/s for a in probs]
+    r = random.uniform(0, 1)
+    for i in range(len(probs)):
+        r = r - probs[i]
+        if r <= 0:
+            return i
+    return len(probs)-1
+
+def c_array(ctype, values):
+    arr = (ctype*len(values))()
+    arr[:] = values
+    return arr
+
+class BOX(Structure):
+    _fields_ = [("x", c_float),
+                ("y", c_float),
+                ("w", c_float),
+                ("h", c_float)]
+
+class DETECTION(Structure):
+    _fields_ = [("bbox", BOX),
+                ("classes", c_int),
+                ("prob", POINTER(c_float)),
+                ("mask", POINTER(c_float)),
+                ("objectness", c_float),
+                ("sort_class", c_int)]
+
+
+class IMAGE(Structure):
+    _fields_ = [("w", c_int),
+                ("h", c_int),
+                ("c", c_int),
+                ("data", POINTER(c_float))]
+
+class METADATA(Structure):
+    _fields_ = [("classes", c_int),
+                ("names", POINTER(c_char_p))]
+
+
+
+#lib = CDLL("/home/nvidia/Desktop/pythonYolo/darknet/libdarknet.so", RTLD_GLOBAL)
+
+# Collect Library DLL
+lib = CDLL("./lib/libdarknet.so", RTLD_GLOBAL)
+lib.network_width.argtypes = [c_void_p]
+lib.network_width.restype = c_int
+lib.network_height.argtypes = [c_void_p]
+lib.network_height.restype = c_int
+
+#---Functions---#
+
+# Predict Network Function
+predict = lib.network_predict
+predict.argtypes = [c_void_p, POINTER(c_float)]
+predict.restype = POINTER(c_float)
+
+# Set Gpu Device
+set_gpu = lib.cuda_set_device
+set_gpu.argtypes = [c_int]
+
+# make image Function
+make_image = lib.make_image
+make_image.argtypes = [c_int, c_int, c_int]
+make_image.restype = IMAGE
+
+# get_network_boxes Function Definition
+get_network_boxes = lib.get_network_boxes
+get_network_boxes.argtypes = [c_void_p, c_int, c_int, c_float, c_float, POINTER(c_int), c_int, POINTER(c_int)]
+get_network_boxes.restype = POINTER(DETECTION)
+
+# Make Network 
+make_network_boxes = lib.make_network_boxes
+make_network_boxes.argtypes = [c_void_p]
+make_network_boxes.restype = POINTER(DETECTION)
+
+# Free Detections Function Definition
+free_detections = lib.free_detections
+free_detections.argtypes = [POINTER(DETECTION), c_int]
+
+# Free Ptrs Function Definition
+free_ptrs = lib.free_ptrs
+free_ptrs.argtypes = [POINTER(c_void_p), c_int]
+
+# network_predict Function Definition
+network_predict = lib.network_predict
+network_predict.argtypes = [c_void_p, POINTER(c_float)]
+
+# reset_rnn Function Definition
+reset_rnn = lib.reset_rnn
+reset_rnn.argtypes = [c_void_p]
+
+# load_net Function Definition
+load_net = lib.load_network
+load_net.argtypes = [c_char_p, c_char_p, c_int]
+load_net.restype = c_void_p
+
+do_nms_obj = lib.do_nms_obj
+do_nms_obj.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+do_nms_sort = lib.do_nms_sort
+do_nms_sort.argtypes = [POINTER(DETECTION), c_int, c_int, c_float]
+
+free_image = lib.free_image
+free_image.argtypes = [IMAGE]
+
+letterbox_image = lib.letterbox_image
+letterbox_image.argtypes = [IMAGE, c_int, c_int]
+letterbox_image.restype = IMAGE
+
+# Load Metadata Function
+load_meta = lib.get_metadata
+lib.get_metadata.argtypes = [c_char_p]
+lib.get_metadata.restype = METADATA
+
+# Load image Function
+load_image = lib.load_image_color
+load_image.argtypes = [c_char_p, c_int, c_int]
+load_image.restype = IMAGE
+
+# RGBR image Function
+rgbgr_image = lib.rgbgr_image
+rgbgr_image.argtypes = [IMAGE]
+
+predict_image = lib.network_predict_image
+predict_image.argtypes = [c_void_p, IMAGE]
+predict_image.restype = POINTER(c_float)
+
+# Convert Image Back
+def convertBack(x, y, w, h):
+    xmin = int(round(x - (w / 2)))
+    xmax = int(round(x + (w / 2)))
+    ymin = int(round(y - (h / 2)))
+    ymax = int(round(y + (h / 2)))
+    return xmin, ymin, xmax, ymax
+
+def array_to_image(arr):
+    # need to return old values to avoid python freeing memory
+    arr = arr.transpose(2,0,1)
+    c, h, w = arr.shape[0:3]
+    arr = np.ascontiguousarray(arr.flat, dtype=np.float32) / 255.0
+    data = arr.ctypes.data_as(POINTER(c_float))
+    im = IMAGE(w,h,c,data)
+    return im, arr
+
+def detect(net, meta, image, thresh=.5, hier_thresh=.5, nms=.45):
+    im, image = array_to_image(image)
+    rgbgr_image(im)
+    num = c_int(0)
+    pnum = pointer(num)
+    predict_image(net, im)
+    dets = get_network_boxes(net, im.w, im.h, thresh,
+                             hier_thresh, None, 0, pnum)
+    num = pnum[0]
+    if nms: do_nms_obj(dets, num, meta.classes, nms)
+
+    res = []
+    for j in range(num):
+        a = dets[j].prob[0:meta.classes]
+        if any(a):
+            ai = np.array(a).nonzero()[0]
+            for i in ai:
+                b = dets[j].bbox
+                res.append((meta.names[i], dets[j].prob[i],
+                           (b.x, b.y, b.w, b.h)))
+
+    res = sorted(res, key=lambda x: -x[1])
+    if isinstance(image, bytes): free_image(im)
+    free_detections(dets, num)
+    return res
+
