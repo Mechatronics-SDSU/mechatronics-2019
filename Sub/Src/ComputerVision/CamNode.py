b7120f4c4d88552078d15cb9378cf8f30b4c86c9
mashafi <ma.shafi99@gmail.com>
2019-07-10 19:25:37 -0700

    This branch is fucking irrelevant anyway

---
+++ Sub/Src/ComputerVision/CamNode.py
@@ -0,0 +1,85 @@
+import numpy as np
+import cv2
+import socket
+import sys
+import struct
+import math
+import io
+import time
+
+# Port Information
+HOST    = '127.0.0.101'
+ADDRESS = 6666 # Doom Eternal
+OURSOCKET=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Data Size per Packet
+MAX_UDP_PACKET_SIZE = 1500
+
+# Encapsulation information
+ENCAPSULATION_0xC0 = bytes.fromhex('c0c0')
+
+# videoCapture from Webcam
+cap = cv2.VideoCapture(1)
+cap.set(3, 450)
+cap.set(4, 450)
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+#    input('press enter to continue...')
+    # Check Video Size
+#    print(frame.shape[:2])
+
+    # print('NO imencode',sys.getsizeof(frame))
+    # Capture Bytes
+    ret, byte_frame = cv2.imencode( '.jpg', frame )
+
+    # Operations on the frame
+
+    '''
+    None: No operations on Frame
+    '''
+
+    # Get the Size of the image
+    image_size = sys.getsizeof(byte_frame)
+
+    # Max Byte Size
+    if image_size:
+
+        imageBuffer = io.BytesIO(byte_frame)
+
+        number_of_packets = math.ceil(image_size/MAX_UDP_PACKET_SIZE)
+        packet_size = math.ceil(image_size/number_of_packets)
+
+        # Uncomment for Manual Control of Send Speed:
+
+#        info_print='''
+#        IMAGE  SIZE:       {}
+#        PACKET SIZE:       {}
+#        NUMBER OF PACKETS: {}
+#        '''.format(image_size, packet_size, number_of_packets)
+
+#        print(info_print)
+#        input('Press Enter to Continue...')
+
+        # Count out the number of packets that need to be sent
+        for count_var in range(0, number_of_packets):
+
+            OURSOCKET.sendto(imageBuffer.read(packet_size), (HOST, ADDRESS))
+
+        # EOF packet for encapsulation
+        OURSOCKET.sendto( ENCAPSULATION_0xC0*2, (HOST, ADDRESS))
+
+        # Image Corruption decreases as sleep time increases (inversely proportional)
+        # this time.sleep is a manual fix balacing speed and the least
+        # amount of corruption on jpegs (not optimal)
+        time.sleep(0) # check as appropriate
+
+    else:
+        time.sleep(0)
+
+
+
+# When everything done, release the capture
+cap.release()
--- Sub/Src/ComputerVision/calculate_distance.py
+++ Sub/Src/ComputerVision/calculate_distance.py
@@ -1,34 +1,112 @@
 import cv2
 import numpy as np
+import time
 
 cam_matrix = np.array([[656.76117331, 0.0, 311.02000055], [0.0, 656.5199208, 235.31118765], [0.0, 0.0, 1.0]])
+print(cam_matrix)
 dist_matrix = np.array([[0.07935776, -0.0490818, -0.0281497, -0.00267198, -1.08707165]])
 
 object_points = np.zeros((6*9, 3), np.float32)
 object_points[:,:2] = np.mgrid[0:9,0:6].T.reshape(-1, 2)
 
-three_dim_points = []
-two_dim_points = []
+#three_dim_points = []
+#two_dim_points = []
 capture = cv2.VideoCapture(1)
 
-while (True):
+while(True):
 
     ret, frame = capture.read()
-    print(ret)
-    #print(frame)
+    #time.sleep()
+    #print(ret)
+
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
     ret2, board_corners = cv2.findChessboardCorners(gray_frame, (9, 6), None)
+    #print(ret2)
+    if ret2:
+        #print(board_corners)
+        #three_dim_points.append(object_points)
+        #two_dim_points.append(board_corners)
+        #print(three_dim_points.shape)
+        #print(two_dim_points.shape)
+        #print(object_points.shape)
+        #print(object_points)
+        #print(board_corners.shape)
+        #print(board_corners)
+        #print(three_dim_points)
+        image_points = board_corners.reshape((board_corners.shape[0], -1), order = 'F')
+        #print(image_points)
+        #print('3-D coordinates: ', object_points)
+        #print('2-D coordinates: ', image_points)
+        #print(image_points.shape)
+
+        working, rvec, tvec = cv2.solvePnP(object_points,
+                               image_points,
+                               cam_matrix,
+                               dist_matrix)
+
+        #print('Rotation vector: ', rvec)
+        print('Translation vector: ', tvec)
+        #rmat = cv2.Rodrigues(rvec)
+        #print('Rotation matrix: ', rmat[0])
+        #print(rmat[0].shape)
+        #print(tvec.shape)
+        #print('Rotation matrix: ', rmat[0])
+
+        #predicted_points = cv2.projectPoints(object_points, rvec, tvec, cam_matrix, dist_matrix)
+        #print('Projected image points: ', predicted_points[0])
+        #print('Actual image points: ', image_points)
+        #movement_matrix = np.concatenate((rmat[0], tvec), axis = 1)
+        #print('Rt matrix: ', movement_matrix)
+        #ones_vector = np.ones((54, 1))
+        #print(ones_vector)
+        #three_dim_points = np.concatenate((object_points, ones_vector), axis = 1)
+        #two_dim_points = np.concatenate((image_points, ones_vector), axis = 1)
+        #print(image_points)
+        #print(two_dim_points)
+        #print(three_dim_points.shape)
+        #print(two_dim_points.shape)
+        #product_A = np.dot(cam_matrix, movement_matrix)
+        #print('Step A: ', product_A)
+        #print(product_A)
+        #print('Transpose: ', three_dim_points.transpose())
+        #print('Transpose shape: ', three_dim_points.transpose().shape)
+        #product_B = np.dot(product_A, three_dim_points.transpose())
+        #print(two_dim_points)
+        #print(product_B)
+        #predicted_points = cv2.projectPoints(t)
+        #print(two_dim_points.shape)
+        #print(product_B.shape)
+        #print('Acutal coordinates: ', two_dim_points[0])
+        #print('Projected coordinates: ', product_B.transpose()[0])
+        #print(product_B)
+        #difference = np.subtract(two_dim_points, np.abs(product_B.transpose()))
+        #print('Errors: ', difference)
+        #im_coordinates = rmat.dot(object_points)
+        #three_dim_points.append(object_points)
+        #two_dim_points.append(board_corners)
+
+        cv2.drawChessboardCorners(frame, (9, 6), board_corners, ret2)
+#        cv2.imshow('picture', picture)
+#        cv2.waitKey(1)
+
+#        cv2.drawChessboardCorners(frame, (9, 6), board_corners, ret)
+
+    cv2.imshow('frame', frame)
+    '''
+    #print(board_corners)
 
     if ret2 == True:
         three_dim_points.append(object_points)
         two_dim_points.append(board_corners)
         rvec, tvec = cv2.solvePnP(three_dim_points, two_dim_points, cam_matrix, dist_matrix)
-        print(rvec)
-        print(tvec)
+        #print(rvec)
+        #print(tvec)
 
         cv2.drawChessboardCorners(frame, (9, 6), board_corners, ret)
         cv2.imshow('picture', frame)
-        cv2.waitKey(1)
+        '''
+    cv2.waitKey(1)
         #three_dim_points.clear()
         #two_dim_points.clear()
 
