25510f68dd00afefe472e14e5939f620a2476a65
Pierce <piercedhowell@gmail.com>
2019-07-18 09:03:37 -0700

    Resolved merge conflicts with the main controller and backplane to be able to merge dev/cv_merge_MPI with development.

--- Sub/Src/MainController/main_controller.py
+++ Sub/Src/MainController/main_controller.py
@@ -1,15 +1,20 @@
-'''
+''
 Copyright 2019, David Pierce Walker-Howell, All rights reserved
-
 Author: David Pierce Walker-Howell<piercedhowell@gmail.com>
 Last Modified 06/5/2019
-
 Description: The main process on the sub that will orchatrate
             everything. This includes the control system, sensor driver,
             and mission commander. Also controls communication with the GUI.
 '''
 import sys
 import os
+
+#SENSOR_HUB_PATH = os.path.join("..", "SensorHub")
+#sys.path.append(SENSOR_HUB_PATH)
+#from sensor_driver import Sensor_Driver
+
+MISSION_COMMANDER_PATH = os.path.join("..", "Mission")
+sys.path.append(MISSION_COMMANDER_PATH)
 
 NAV_CONT_PATH = os.path.join("..", "Dynamics")
 sys.path.append(NAV_CONT_PATH)
@@ -19,13 +24,11 @@ SENSOR_HUB_PATH = os.path.join("..", "SensorHub")
 sys.path.append(SENSOR_HUB_PATH)
 from sensor_driver import Sensor_Driver
 
-VISION_PATH = os.path.join("..", "ComputerVision")
-sys.path.append(VISION_PATH)
-from vision import Vision
-
 from message_passing.Nodes.node_base_udp import node_base
 import time
 import socket
+
+from mission_commander import Mission_Commander
 
 class Main_Controller(node_base):
     '''
@@ -33,11 +36,9 @@ class Main_Controller(node_base):
     def __init__(self, MEM, IP):
         '''
         Intialize the main controller of the sub.
-
         Parameters:
             MEM: local dictionary containing all data for local message transfer
             IP: dictionary containing address, sockets, etc for network transfer
-
         Returns:
             N/A
         '''
@@ -51,22 +52,24 @@ class Main_Controller(node_base):
 
         self.sensor_driver = Sensor_Driver()
         self.navigation_controller = Navigation_Controller(MEM, IP, self.sensor_driver)
-        
-        #Initializing vision thread
+        self.mission_commander = Mission_Commander(self.sensor_driver)
+
         self.run_main_controller = True
-       
+
         #Start up threads
         self.sensor_driver.start()
         self.navigation_controller.start()
+        self.mission_commander.start()
+
+        #Initialize the current position to (0, 0)
+        self.sensor_driver.zero_pos()
 
     def print_sensor_data(self, sensor_data):
         '''
         Format and print out the sensor data to the command window.
-
         Parameters:
             sensor_data: A list of the sensor data.
                         [roll, pitch, yaw, x_pos, y_pos, depth]
-
         Returns:
             N/A
         '''
@@ -86,13 +89,12 @@ class Main_Controller(node_base):
         Run the Main controller of the sub
         '''
         while(self.run_main_controller):
-            continue
+            time.sleep(0.1)
 
 if __name__ == "__main__":
 
     rc_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     thrust_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    camera_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     ip_address = ('192.168.1.14', 6312)
     thrust_socket.bind((ip_address))
 
@@ -101,16 +103,9 @@ if __name__ == "__main__":
             'address': ip_address,
             'sockets': (rc_socket, thrust_socket),
             'type': 'UDP'
-            },
-        'CAMERA':
-            {
-            'address': ('192.168.1.1', 6969),
-            'sockets': (camera_socket, None),
-            'type': 'UDP'
             }
         }
-
     MEM={'RC':b'\x00\x01\x807\x00\x00\x00\x00\x00\x01\x807\x00\x00\x00\x00\x00\x01\x807\x00\x00\x00\x00\x00\x01\x807\x00\x00\x00\x00'}
 
     main_controller = Main_Controller(MEM, IP)
-    main_controller.start()
+main_controller.start()
