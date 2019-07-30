6cd4f01687658298d4c32128480d8b071be661d2
mashafi <ma.shafi99@gmail.com>
2019-07-29 22:31:44 -0700

    Made some changes to the mission planner script to see if file transfer works. will be testing on the tegra

--- GUI/Src/missionPlanner.py
+++ GUI/Src/missionPlanner.py
@@ -1,24 +1,35 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 import os
+import pysftp
 from PyQt5 import uic
 
 class MissionPlanner(QtWidgets.QWidget):
-    
-    def __init__(self):
+
+    def __init__(self, host, username, password):
 
         QtWidgets.QWidget.__init__(self)
 
+        self.host = host
+        self.username = username
+        self.password = password
+        self.server_connection = pysftp.Connection(host=self.host, username=self.username, password=self.password)
+        self.foreign_filepath = "mechatronics-2019/Sub/Src/Mission/MissionFiles/"
+        self.local_filepath = None
+        self.file_list = None
+
         #Call in the ui for mission select
         self.task_selector_widget = uic.loadUi("task_selector_widget.ui", self)
-        self.task_selector_widget.comboBox.currentIndexChanged.connect(self.taskSelected)
-        self.task_selector_widget.findFile.clicked.connect(self.getFile)
+        self.task_selector_widget.selectorBox.currentIndexChanged.connect(self.taskSelected)
+        self.task_selector_widget.findFileButton.clicked.connect(self.getFile)
+        self.task_selector_widget.sendButton.clicked.connect(self.send_file)
+        self.task_selector_widget.receiveButton.clicked.connect(self.receive_file)
 
     def taskSelected(self):
 
-        if self.task_selector_widget.comboBox.currentIndex() == 2:
+        if self.task_selector_widget.selectorBox.currentIndex() == 2:
             self.set_gate_no_vision_selected()
-        elif self.task_selector_widget.comboBox.currentIndex() == 0:
+        elif self.task_selector_widget.selectorBox.currentIndex() == 0:
             self.waypoint_task_selected()
 
     def set_gate_no_vision_selected(self):
@@ -33,8 +44,26 @@ class MissionPlanner(QtWidgets.QWidget):
 
     def getFile(self):
 
-        self.dlg = QtWidgets.QFileDialog()
-        self.dlg.setFileMode(QtWidgets.QFileDialog.AnyFile)
+        self.local_filepath = str(self.task_selector_widget.findFileEditor.text())
+
+    def send_file(self):
+        index = self.mission_file.rfind('/')
+        try:
+            self.server_connection.put(self.mission_file, self.foreign_filepath + (self.mission_file[index::]))
+        except Exception as e:
+            print("[ERROR]: File not found!!!", e)
+
+    def receive_file(self):
+        self.file_list = self.server_connection.listdir(self.foreign_filepath)
+        for filename in self.file_list:
+            if filename.endswith('.json'):
+                try:
+                    self.server_connection.get(self.foreign_filepath + filename, self.local_filepath + filename)
+                except Exception as e:
+                    print("[ERROR]: Filepath not found!!!", e)
+
+    def kill_connection(self):
+        self.server_connection.close()
 
 class SetGate(QtWidgets.QWidget):
 
@@ -56,13 +85,12 @@ class WaypointTask(QtWidgets.QWidget):
         self.waypoint_task = uic.loadUi("waypoint_task_widget.ui", self)
         self.waypoint_task.show()
 
-
 if __name__ == "__main__":
 
     main_app = QtWidgets.QApplication([])
     main_app.setStyle('Fusion')
 
-    main_widget = MissionPlanner()
+    main_widget = MissionPlanner("192.168.1.14", "nvidia", "nvidia")
     main_widget.resize(1000, 1000)
 
     main_widget.show()
