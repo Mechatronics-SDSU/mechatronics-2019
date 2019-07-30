108065921cd7e90822fe61f6fe8e085948e42acc
mashafi <ma.shafi99@gmail.com>
2019-07-29 19:29:49 -0700

    Figured out the send part as well

--- GUI/Src/server_connection.py
+++ GUI/Src/server_connection.py
@@ -37,6 +37,11 @@ def main():
     local_path = input("Where should I send it to? ")
     print(local_path)
     file_transferer.receive_file(remote_path, local_path)
+    local_path_2 = input("Now what file would you like to send? ")
+    print(local_path_2)
+    remote_path_2 = input("And where would you like to send it? ")
+    print(remote_path_2)
+    file_transferer.send_file(local_path_2, remote_path_2)
 
 
 if __name__ == '__main__':
