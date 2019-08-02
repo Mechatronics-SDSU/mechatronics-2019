448fe3e86739925a0cc0812038c7983fa4b02dc6
Pierce <piercedhowell@gmail.com>
2019-08-01 19:56:13 -0700

    Finished the waypoint generation from satellite images. Now you can move previous waypoints.

--- GUI/Src/WaypointMapping/generate_waypoint_map.py
+++ GUI/Src/WaypointMapping/generate_waypoint_map.py
@@ -22,6 +22,7 @@ class Generate_Waypoint_Map:
         self.temporary_map_image = self.original_map_image.copy() #This is where stuff will be drawn intermediately
         self.waypointed_map_image = self.original_map_image.copy() #The map image with the drawn waypoints
 
+        self.current_editing_index = 0
         with open(map_json, 'r') as read_map_json:
             map_data = json.load(read_map_json)
 
@@ -29,13 +30,11 @@ class Generate_Waypoint_Map:
         self.north_angle = map_data["north_angle"]
 
         #State of drawing the points on the image.
-        self.drawing = False
+        self.placing_new_waypoint = False
 
         cv2.namedWindow('Map_Image')
         cv2.setMouseCallback("Map_Image", self.collect_waypoint_callback)
 
-        #The pixel that is considered the origin
-        self.origin_pixel = [0.0, 0.0]
 
         #The waypoint list relative to the origin. This is the North/East posisition
         self.waypoint_list = []
@@ -44,22 +43,39 @@ class Generate_Waypoint_Map:
         self.origin_color = (0, 255, 0)
         self.waypoint_color = (255, 0, 255)
 
-        self.setting_origin = True
+        self.moving_previous_waypoint = True
+        self.previous_waypoint_selected = False
+        self.point_hoverd_over = None
 
-    def set_map(self, map_image, map_json):
+        #If the waypoint map has a previously defined origin, use that. Else the user should pick one as the first point.
+        #If the map json file does not have the static origin parameter, the FIRST waypoint placed will be the origin
+        if "static_origin" in map_data.keys():
+            self.waypoint_list.append(map_data["static_origin"] + [0.0, 0.0])
+            self.current_editing_index = 1
+        else:
+            self.waypoint_list.append([0.0, 0.0, 0.0, 0.0])
+            self.current_editing_index = 0
+
+        self.placing_new_waypoint = False
+
+        self.redraw_all_points()
+
+
+    def set_map(self, map_image, map_json, waypoint_save_file):
         '''
-        Reload a new map given the location to the map image
+        Initialize the waypoint map generator.
 
         Parameters:
-            map_image: The location of the google maps image
-        Returns:
-            N/A
+            map_image: The location to the google maps image.
+            map_json: The json file containing the pixel_distance ratio and
+                        the north angle.
         '''
         #Keep an original map image in case you delete a waypoint
         self.original_map_image = cv2.imread(map_image)
         self.temporary_map_image = self.original_map_image.copy() #This is where stuff will be drawn intermediately
         self.waypointed_map_image = self.original_map_image.copy() #The map image with the drawn waypoints
 
+        self.current_editing_index = 0
         with open(map_json, 'r') as read_map_json:
             map_data = json.load(read_map_json)
 
@@ -67,28 +83,82 @@ class Generate_Waypoint_Map:
         self.north_angle = map_data["north_angle"]
 
         #State of drawing the points on the image.
-        self.drawing = False
-
-        #The pixel that is considered the origin
-        self.origin_pixel = [0.0, 0.0]
+        self.placing_new_waypoint = False
 
         #The waypoint list relative to the origin. This is the North/East posisition
         self.waypoint_list = []
+        self.waypoint_save_file = waypoint_save_file
 
-        self.setting_origin = True
+        self.origin_color = (0, 255, 0)
+        self.waypoint_color = (255, 0, 255)
+
+        self.moving_previous_waypoint = True
+        self.previous_waypoint_selected = False
+        self.point_hoverd_over = None
+
+        #If the waypoint map has a previously defined origin, use that. Else the user should pick one as the first point.
+        #If the map json file does not have the static origin parameter, the FIRST waypoint placed will be the origin
+        if "static_origin" in map_data.keys():
+            self.waypoint_list.append(map_data["static_origin"] + [0.0, 0.0])
+            self.current_editing_index = 1
+        else:
+            self.waypoint_list.append([0.0, 0.0, 0.0, 0.0])
+            self.current_editing_index = 0
+
+        self.placing_new_waypoint = False
+
+        self.redraw_all_points()
+
+    def redraw_all_points(self):
+        '''
+        Redraw all the points with their given connections from the self.waypoint_list file.
+
+        Parameters:
+            N/A
+
+        Returns:
+            N/A
+        '''
+        #Reset the image to the original, undrawn copy
+        self.waypointed_map_image = self.original_map_image.copy()
+        self.temporary_map_image = self.waypointed_map_image.copy()
+
+        #Temporary waypoints list, clear out the old so the calculate positions function can rebuild it.
+        temp_waypoint_list = self.waypoint_list.copy()
+        self.waypoint_list = []
+        for index, waypoint in enumerate(temp_waypoint_list):
+
+            #The first waypoint in the waypoint list is the origin, so it should be set to setting origin mode
+            if(index == 0):
+                color = self.origin_color
+                self.waypoint_list.append([waypoint[0], waypoint[1], 0, 0])
+            else:
+                color = self.waypoint_color
+
+            cv2.circle(self.waypointed_map_image, (waypoint[0], waypoint[1]), 6, color, -1)
+            if(index > 0):
+
+
+                cv2.line(self.waypointed_map_image, prev_coordinates, (waypoint[0], waypoint[1]), (36, 238, 231), 3)
+                self.waypoint_list.append(self.calculate_position(waypoint[0], waypoint[1]))
+
+            prev_coordinates = (waypoint[0], waypoint[1])
+        self.temporary_map_image = self.waypointed_map_image.copy()
+
+
 
     def calculate_position(self, x_coordinate, y_coordinate):
         '''
         '''
         north_angle_radians = math.radians(self.north_angle)
-        north_position = ((self.origin_pixel[0] - y_coordinate)*math.cos(north_angle_radians) + \
-                         (x_coordinate - self.origin_pixel[1])*-1*math.sin(north_angle_radians)) / self.pixel_distance_ratio
+        north_position = (((self.waypoint_list[0])[1] - y_coordinate)*math.cos(north_angle_radians) + \
+                         (x_coordinate - (self.waypoint_list[0])[0])*-1*math.sin(north_angle_radians)) / self.pixel_distance_ratio
 
-        east_position = ((self.origin_pixel[0] - y_coordinate)*math.sin(north_angle_radians) + \
-                         (x_coordinate - self.origin_pixel[1])*math.cos(north_angle_radians)) / self.pixel_distance_ratio
+        east_position = (((self.waypoint_list[0])[1] - y_coordinate)*math.sin(north_angle_radians) + \
+                         (x_coordinate - (self.waypoint_list[0])[0])*math.cos(north_angle_radians)) / self.pixel_distance_ratio
 
-        self.waypoint_list.append([x_coordinate, y_coordinate, north_position, east_position])
-        print(north_position, east_position)
+
+        return([x_coordinate, y_coordinate, north_position, east_position])
 
     def collect_waypoint_callback(self, event, x_coordinate, y_coordinate, flags, param):
         '''
@@ -103,72 +173,121 @@ class Generate_Waypoint_Map:
         '''
         self.waypointed_map_image = self.temporary_map_image.copy()
 
-        if(self.setting_origin):
-            color = self.origin_color
-        else:
-            color = self.waypoint_color
-
+        #If the left button on the mouse is clicked, then a waypoint is going to pop up
+        #and can be dragged around until the left button is not held down.
         if(event == cv2.EVENT_LBUTTONDOWN):
-            self.drawing = True
-            self.x_coordinate = x_coordinate
-            self.y_coordinate = y_coordinate
+            self.placing_new_waypoint = True
+            self.changed_point_index = None
+            #First check to see if already hovering over a waypoint
+            for index, waypoint in enumerate(self.waypoint_list):
+                mouse_distance_from_waypoint = math.sqrt((x_coordinate - waypoint[0])**2 + (y_coordinate - waypoint[1])**2)
+
+                if(mouse_distance_from_waypoint <= 5):
+                    self.changed_point_index = index
+                    break
+            #If moving an already existing point
+            if(self.changed_point_index != None):
+
+                #If redrawing origin
+                if(self.changed_point_index == 0):
+                    self.waypoint_list[self.changed_point_index] = [x_coordinate, y_coordinate, 0.0, 0.0]
+
+                else:
+                    self.waypoint_list[self.changed_point_index] = self.calculate_position(x_coordinate, y_coordinate)
+
+                self.redraw_all_points()
+
+            #If a new point
+            else:
+
+                cv2.circle(self.waypointed_map_image, (x_coordinate, y_coordinate), 5, self.waypoint_color, -1)
+                #self.waypoint_list.append(self.calculate_position(x_coordinate, y_coordinate))
 
 
-            cv2.circle(self.waypointed_map_image, (x_coordinate, y_coordinate), 5, color, -1)
 
+        #If the mouse is moving (while the left button is held down) and drawing mode is on
+        #then the point can be dragged around the screen
         elif event == cv2.EVENT_MOUSEMOVE:
-            if(self.drawing == True):
-                cv2.circle(self.waypointed_map_image, (x_coordinate, y_coordinate), 5, color, -1)
+            if(self.placing_new_waypoint):
 
+                if(self.changed_point_index != None):
+
+                    if(self.changed_point_index == 0):
+
+                        self.waypoint_list[self.changed_point_index] = [x_coordinate, y_coordinate, 0.0, 0.0]
+
+                    else:
+                        self.waypoint_list[self.changed_point_index] = self.calculate_position(x_coordinate, y_coordinate)
+
+                    self.redraw_all_points()
+                else:
+                    cv2.circle(self.waypointed_map_image, (x_coordinate, y_coordinate), 5, self.waypoint_color, -1)
+                    #self.waypoint_list.append(self.calculate_position(x_coordinate, y_coordinate))
+
+
+        #When the left mouse button is un-clicked after being held down, place the waypoint in this location.
         elif event == cv2.EVENT_LBUTTONUP:
+            if(self.placing_new_waypoint):
+                self.placing_new_waypoint = False
+                if(self.changed_point_index != None):
 
-            if(self.drawing == True):
+                    if(self.changed_point_index == 0):
+                        self.waypoint_list[self.changed_point_index] = [x_coordinate, y_coordinate, 0.0, 0.0]
 
-                if(self.setting_origin):
-                    self.origin_pixel = [y_coordinate, x_coordinate]
-                    self.setting_origin = False
-                self.drawing = False
-                cv2.circle(self.waypointed_map_image, (x_coordinate, y_coordinate), 5, color, -1)
-                print(x_coordinate, y_coordinate)
-                #Draw line between points
-                if(len(self.waypoint_list) > 0):
-                    prev_coordinates = tuple((self.waypoint_list[-1])[0:2])
-                    cv2.line(self.waypointed_map_image, prev_coordinates, (x_coordinate, y_coordinate), (0, 0, 255), 1)
+                    else:
+                        self.waypoint_list[self.changed_point_index] = self.calculate_position(x_coordinate, y_coordinate)
+
+                    self.redraw_all_points()
+                    self.changed_point_index = None
+                else:
+                    cv2.circle(self.waypointed_map_image, (x_coordinate, y_coordinate), 5, self.waypoint_color, -1)
+
+                    if(len(self.waypoint_list) > 0):
+                        prev_coordinates = tuple(self.waypoint_list[-1][0:2])
+                        cv2.line(self.waypointed_map_image, prev_coordinates, (x_coordinate, y_coordinate), (36, 238, 231), 3)
+
+                    self.waypoint_list.append(self.calculate_position(x_coordinate, y_coordinate))
 
                 self.temporary_map_image = self.waypointed_map_image.copy()
-
-                self.calculate_position(x_coordinate, y_coordinate)
 
     def run(self):
         '''
         '''
         while(1):
+
             cv2.imshow("Map_Image", self.waypointed_map_image)
             k = cv2.waitKey(1) & 0xFF
 
             if(k == 27):
                 break
-            if(k == ord('o')):
-                print("Setting Origin Picked")
-                self.setting_origin = True
+            #Press z to remove last set waypoint
+            if(k == ord('z')):
+
+                if(len(self.waypoint_list) > 1):
+
+                    del self.waypoint_list[-1]
+                    self.redraw_all_points()
 
             if(k == ord('s')): #Save the points to a waypoint file
                 print("Saving Waypoints")
 
                 if(len(self.waypoint_list) > 1):
 
+                    #Open the waypoints csv file
                     with open(self.waypoint_save_file, 'w') as write_waypoints_file:
                         waypoints_csv_writer = csv.writer(write_waypoints_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
+                        #Iterate through each waypoint an save it.
                         for index, waypoint in enumerate(self.waypoint_list[1:]):
                             waypoints_csv_writer.writerow([index, waypoint[2], waypoint[3], 4])
 
 
 
+
 if __name__ == "__main__":
-    map_image = "Maps/aquaplex.png"
-    map_json = "Maps/aquaplex.json"
-    waypoint_save_file = "waypoint.csv"
+    map_image = "Maps/transdec_pool_b.png"
+    map_json = "Maps/transdec_pool_b.json"
+    waypoint_save_file = "waypoints.csv"
 
     generate_waypoint_map = Generate_Waypoint_Map(map_image, map_json, waypoint_save_file)
     generate_waypoint_map.run()
